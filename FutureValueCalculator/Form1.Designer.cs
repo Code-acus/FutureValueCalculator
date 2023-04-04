d40e56ddcadf5ba4b5c1318ed2717761f83fdc06
Harrison <47501747+Code-acus@users.noreply.github.com>
2023-04-04 15:00:04 -0600

    Added Gui
    No coed just GUI

--- FutureValueCalculator/Form1.Designer.cs
+++ FutureValueCalculator/Form1.Designer.cs
@@ -28,13 +28,220 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblMonthlyInvestment = new System.Windows.Forms.Label();
+            this.lblNumberOfYears = new System.Windows.Forms.Label();
+            this.llblYearlyInvestment = new System.Windows.Forms.Label();
+            this.lblFutureValue = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtMonthlyInvestment = new System.Windows.Forms.TextBox();
+            this.txtInterestRate = new System.Windows.Forms.TextBox();
+            this.txtYears = new System.Windows.Forms.TextBox();
+            this.txtFutureValue = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblMonthlyInvestment
+            // 
+            this.lblMonthlyInvestment.AutoSize = true;
+            this.lblMonthlyInvestment.Location = new System.Drawing.Point(79, 107);
+            this.lblMonthlyInvestment.Name = "lblMonthlyInvestment";
+            this.lblMonthlyInvestment.Size = new System.Drawing.Size(123, 16);
+            this.lblMonthlyInvestment.TabIndex = 0;
+            this.lblMonthlyInvestment.Text = "Monthly Investment:";
+            // 
+            // lblNumberOfYears
+            // 
+            this.lblNumberOfYears.AutoSize = true;
+            this.lblNumberOfYears.Location = new System.Drawing.Point(79, 198);
+            this.lblNumberOfYears.Name = "lblNumberOfYears";
+            this.lblNumberOfYears.Size = new System.Drawing.Size(111, 16);
+            this.lblNumberOfYears.TabIndex = 1;
+            this.lblNumberOfYears.Text = "Number of Years:";
+            // 
+            // llblYearlyInvestment
+            // 
+            this.llblYearlyInvestment.AutoSize = true;
+            this.llblYearlyInvestment.Location = new System.Drawing.Point(79, 153);
+            this.llblYearlyInvestment.Name = "llblYearlyInvestment";
+            this.llblYearlyInvestment.Size = new System.Drawing.Size(116, 16);
+            this.llblYearlyInvestment.TabIndex = 2;
+            this.llblYearlyInvestment.Text = "Yearly Investment:";
+            // 
+            // lblFutureValue
+            // 
+            this.lblFutureValue.AutoSize = true;
+            this.lblFutureValue.Location = new System.Drawing.Point(79, 241);
+            this.lblFutureValue.Name = "lblFutureValue";
+            this.lblFutureValue.Size = new System.Drawing.Size(79, 16);
+            this.lblFutureValue.TabIndex = 3;
+            this.lblFutureValue.Text = "FutureValue";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(661, 147);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 16);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(661, 192);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(44, 16);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "label6";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(661, 241);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(44, 16);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "label7";
+            // 
+            // txtMonthlyInvestment
+            // 
+            this.txtMonthlyInvestment.Location = new System.Drawing.Point(285, 107);
+            this.txtMonthlyInvestment.Name = "txtMonthlyInvestment";
+            this.txtMonthlyInvestment.Size = new System.Drawing.Size(100, 22);
+            this.txtMonthlyInvestment.TabIndex = 7;
+            // 
+            // txtInterestRate
+            // 
+            this.txtInterestRate.Location = new System.Drawing.Point(285, 153);
+            this.txtInterestRate.Name = "txtInterestRate";
+            this.txtInterestRate.Size = new System.Drawing.Size(100, 22);
+            this.txtInterestRate.TabIndex = 9;
+            // 
+            // txtYears
+            // 
+            this.txtYears.Location = new System.Drawing.Point(285, 198);
+            this.txtYears.Name = "txtYears";
+            this.txtYears.Size = new System.Drawing.Size(100, 22);
+            this.txtYears.TabIndex = 11;
+            // 
+            // txtFutureValue
+            // 
+            this.txtFutureValue.Location = new System.Drawing.Point(285, 241);
+            this.txtFutureValue.Name = "txtFutureValue";
+            this.txtFutureValue.ReadOnly = true;
+            this.txtFutureValue.Size = new System.Drawing.Size(100, 22);
+            this.txtFutureValue.TabIndex = 12;
+            this.txtFutureValue.TabStop = false;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(795, 147);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 22);
+            this.textBox7.TabIndex = 13;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(795, 192);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(100, 22);
+            this.textBox8.TabIndex = 14;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(795, 235);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(100, 22);
+            this.textBox9.TabIndex = 15;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(451, 368);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(106, 36);
+            this.btnCalculate.TabIndex = 16;
+            this.btnCalculate.Text = "&Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Location = new System.Drawing.Point(712, 368);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(106, 36);
+            this.btnExit.TabIndex = 17;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(582, 368);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(106, 36);
+            this.button3.TabIndex = 18;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnCalculate;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(944, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.txtFutureValue);
+            this.Controls.Add(this.txtYears);
+            this.Controls.Add(this.txtInterestRate);
+            this.Controls.Add(this.txtMonthlyInvestment);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblFutureValue);
+            this.Controls.Add(this.llblYearlyInvestment);
+            this.Controls.Add(this.lblNumberOfYears);
+            this.Controls.Add(this.lblMonthlyInvestment);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Future Value";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblMonthlyInvestment;
+        private System.Windows.Forms.Label lblNumberOfYears;
+        private System.Windows.Forms.Label llblYearlyInvestment;
+        private System.Windows.Forms.Label lblFutureValue;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtMonthlyInvestment;
+        private System.Windows.Forms.TextBox txtInterestRate;
+        private System.Windows.Forms.TextBox txtYears;
+        private System.Windows.Forms.TextBox txtFutureValue;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button button3;
     }
 }
 
